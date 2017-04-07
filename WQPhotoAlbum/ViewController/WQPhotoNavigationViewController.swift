1a803eb68914b35edb544ffdcbc8cdec4620d16c
王前 <W_qian@wangqiandeMacBook-Pro.local>
2017-04-07 17:39:33 +0800

    []

---
+++ WQPhotoAlbum/ViewController/WQPhotoNavigationViewController.swift
@@ -0,0 +1,35 @@
+//
+//  WQPhotoNavigationViewController.swift
+//  WQPhotoAlbum
+//
+//  Created by 王前 on 2017/4/7.
+//  Copyright © 2017年 qian.com. All rights reserved.
+//
+
+import UIKit
+
+class WQPhotoNavigationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
